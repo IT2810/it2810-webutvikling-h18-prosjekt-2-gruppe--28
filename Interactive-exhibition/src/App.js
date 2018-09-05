52641be991c98cf1768d607c838597e48801754d
Sigbjørn Skauge <sigbjorn.skauge@outlook.com>
2018-09-05 14:53:42 +0200

    #4 made static page

--- Interactive-exhibition/src/App.js
+++ Interactive-exhibition/src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PoemShowcase from './components/PoemShowcase.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="grid-container">
+          <div class="grid-item" id="item1">
+              item1
+          </div>
+
+          <div class="grid-item" id="item3">
+
+          </div>
+
+          <div class="grid-item" id="item2">
+              <div class="tab" id="tab-1"> tab 1</div>
+              <div class="tab" id="tab-2"> tab 2 </div>
+              <div class="tab" id="tab-3"> tab 3 </div>
+              <div class="tab" id="tab-4"> tab 4 </div>
+          </div>
+
+          <div class="grid-item" id="item4">
+              item4
+          </div>
+
+          <div class="grid-item" id="item5">
+                  <PoemShowcase />
+          </div>
+
       </div>
     );
   }
---
+++ Interactive-exhibition/src/components/PoemShowcase.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PoemShowcase extends Component {
+  render() {
+    return (
+      <div class="poemHolder">
+        <h1>Load Title</h1>
+        <p id="author">Load Author</p>
+        <p> Load Poem</p>
+      </div>
+    );
+  }
+}
+
+export default PoemShowcase;
