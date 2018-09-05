4ef47d46aa659b9abc44390a2b4879c8eb9ed411
Unknown <sander-sin@hotmail.com>
2018-09-05 15:55:11 +0200

    #5 started with module

--- Interactive-exhibition/src/App.js
+++ Interactive-exhibition/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ImageContainer from './ImageContainer.js';
+
+
 
 class App extends Component {
   render() {
@@ -10,12 +13,17 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div className="imageContainer">
+          <ImageContainer />
+          </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started2, edit <code>src/App.js</code> and save to reload.
+          <ImageContainer />
         </p>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ Interactive-exhibition/src/ImageContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ImageContainer extends Component {
+    render() {
+      return (
+        <div>
+        <p> Hello!</p>
+        </div>
+      );
+    }
+  }
+
+  export default ImageContainer;
