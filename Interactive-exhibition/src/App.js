5d85370067183a32c36d48c523e72040b97e6df3
Unknown <sander-sin@hotmail.com>
2018-09-12 15:27:13 +0200

    #8 Added state to ArtContainer

--- Interactive-exhibition/src/App.js
+++ Interactive-exhibition/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ArtContainer from './ArtContainer.js';
 import './App.css';
+import elephant from './assets/img/animal/Elephant.svg';
+import laugh from './assets/sound/human/laugh.mp3';
+const poem = "Sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. Malesuada nunc vel risus commodo viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Pellentesque dignissim enim sit amet venenatis urna cursus eget. Imperdiet massa tincidunt nunc pulvinar sapien et. Ultrices vitae auctor eu augue ut lectus arcu. Amet dictum sit amet justo donec. Volutpat diam ut venenatis tellus in metus vulputate eu. Vitae turpis massa sed elementum tempus. Sem et tortor consequat id porta. Semper quis lectus nulla at volutpat diam ut venenatis. Justo donec enim diam vulputate. Fermentum leo vel orci porta non pulvinar neque. Bibendum enim facilisis gravida neque convallis a. Iaculis nunc sed augue lacus viverra vitae. A erat nam at lectus. Pulvinar sapien et ligula ullamcorper malesuada proin libero. Lectus vestibulum mattis ullamcorper velit sed. Praesent tristique magna sit amet";
 
 function Category(props){
 
@@ -97,7 +100,7 @@ class App extends Component {
         </div>  
 
         <div class="grid-item" id="item4">
-          <ArtContainer/>
+          <ArtContainer imageSource={elephant} poemContent={poem} audioSource={laugh}/>
         </div>
       </div>
         
--- Interactive-exhibition/src/ArtContainer.js
+++ Interactive-exhibition/src/ArtContainer.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import elephant from './assets/img/animal/Elephant.svg';
-import laugh from './assets/sound/human/laugh.mp3';
-const poem = "Sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. Malesuada nunc vel risus commodo viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Pellentesque dignissim enim sit amet venenatis urna cursus eget. Imperdiet massa tincidunt nunc pulvinar sapien et. Ultrices vitae auctor eu augue ut lectus arcu. Amet dictum sit amet justo donec. Volutpat diam ut venenatis tellus in metus vulputate eu. Vitae turpis massa sed elementum tempus. Sem et tortor consequat id porta. Semper quis lectus nulla at volutpat diam ut venenatis. Justo donec enim diam vulputate. Fermentum leo vel orci porta non pulvinar neque. Bibendum enim facilisis gravida neque convallis a. Iaculis nunc sed augue lacus viverra vitae. A erat nam at lectus. Pulvinar sapien et ligula ullamcorper malesuada proin libero. Lectus vestibulum mattis ullamcorper velit sed. Praesent tristique magna sit amet";
+
+
+
 
 
 function ImageContainer(props) {
@@ -32,9 +32,19 @@ function AudioContainer(props) {
 }
 
 class ArtContainer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        imageSource : props.imageSource,
+        poemContent : props.poemContent,
+        audioSource : props.audioSource,
+    };
+
+  }
     
-    renderImageContainer(image1) {
-        return <ImageContainer imageSource={image1}>
+    renderImageContainer(imageSource) {
+        return <ImageContainer imageSource={imageSource}>
         </ImageContainer>;
     }
 
@@ -51,9 +61,9 @@ class ArtContainer extends Component {
     render() {
       return (
             <div class="artContainer">
-            {this.renderImageContainer(elephant)}
-            {this.renderPoemContainer(poem)}
-            {this.renderAudioContainer(laugh)}
+            {this.renderImageContainer(this.state.imageSource)}
+            {this.renderPoemContainer(this.state.poemContent)}
+            {this.renderAudioContainer(this.state.audioSource)}
             </div>
       );
     }
