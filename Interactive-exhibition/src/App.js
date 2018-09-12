869300f00c7a90a8f0971510db374a700dd5ef93
Sigbjørn Skauge <sigbjorn.skauge@outlook.com>
2018-09-12 11:30:36 +0200

    #4 Made static page that loads poems from json
    Json currently needs its own server

--- Interactive-exhibition/src/App.js
+++ Interactive-exhibition/src/App.js
@@ -1,32 +1,31 @@
 import React, { Component } from 'react';
 import PoemShowcase from './components/PoemShowcase.js';
-import './contentLoader.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div class="grid-container">
-          <div class="grid-item" id="item1">
+      <div className="grid-container">
+          <div className="grid-item" id="item1">
               item1
           </div>
 
-          <div class="grid-item" id="item3">
+          <div className="grid-item" id="item3">
 
           </div>
 
-          <div class="grid-item" id="item2">
-              <div class="tab" id="tab-1"> tab 1</div>
-              <div class="tab" id="tab-2"> tab 2 </div>
-              <div class="tab" id="tab-3"> tab 3 </div>
-              <div class="tab" id="tab-4"> tab 4 </div>
+          <div className="grid-item" id="item2">
+              <div className="tab" id="tab-1"> tab 1</div>
+              <div className="tab" id="tab-2"> tab 2 </div>
+              <div className="tab" id="tab-3"> tab 3 </div>
+              <div className="tab" id="tab-4"> tab 4 </div>
           </div>
 
-          <div class="grid-item" id="item4">
+          <div className="grid-item" id="item4">
               item4
           </div>
 
-          <div class="grid-item" id="item5">
+          <div className="grid-item" id="item5">
                   <PoemShowcase />
           </div>
 
--- Interactive-exhibition/src/components/PoemShowcase.js
+++ Interactive-exhibition/src/components/PoemShowcase.js
@@ -1,24 +1,66 @@
 import React, { Component } from 'react';
 
 class PoemShowcase extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      poem: {}
+  constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: []
+      };
     }
+
+    componentDidMount() {
+    this.setState({ isLoading: true });
+
+    fetch("http://localhost:3001/poems")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.nature
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   render() {
-    console.log(this.state.poem);
-    return (
-      <div class="poemHolder">
-        <h1>Load Title</h1>
-        <p id="author">Load Author</p>
-
-        <p> Load Poem</p>
-      </div>
-    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          {items.map(item => (
+            <h1>
+              {item.poem1.title}
+            </h1>
+          ))}
+          {items.map(item => (
+            <p>
+              {item.poem1.author}
+            </p>
+          ))}
+          {items.map(item => (
+            <p>
+              {item.poem1.content}
+            </p>
+          ))}
+        </div>
+      );
+    }
   }
 }
 
